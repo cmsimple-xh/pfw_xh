0ad49a9043e87755d2cc9c341fa1bdc599938d03
Olaf Penschke <35469568+olape-git@users.noreply.github.com>
2025-02-07 12:26:04 +0100

    Preparation for release

--- tests/unit/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once './vendor/autoload.php';
-require_once './classes/required_classes.php';
-require_once '../../cmsimple/functions.php';
-require_once '../../cmsimple/adminfuncs.php';
-require_once './tests/unit/TestCase.php';
-require_once './tests/unit/Foo.php';
